a6acfaed43f9274765fd8a06bac76b4841b0defc
Arturo Aparicio <arturoa@arturoa-mac.local>
2019-02-07 15:52:51 -0800

    Added init script for Jenkins

---
+++ init-jenkins.sh
@@ -0,0 +1,9 @@
+#/bin/sh
+if [ ! -f /var/lib/jenkins/init.txt ]; then
+    echo "First time setup of Jenkins container.... this may take a few minutes"
+    cd /tmp
+    wget https://dl.bintray.com/jfrog/enterprise-trial-installer/jfrog/jenkins/jenkins-1.0.21.zip
+    unzip jenkins-1.0.21.zip
+    mv /tmp/jfrog/data/jenkins/ /var/lib/jenkins/
+    touch /var/lib/jenkins/init.txt
+fi
