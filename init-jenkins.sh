7242f52e5a0a377852c27d19258716822a06a784
Arturo Aparicio <arturoa@arturoa-mac.local>
2019-02-07 16:46:45 -0800

    Fixes to set up Jenkins

--- init-jenkins.sh
+++ init-jenkins.sh
@@ -1,9 +1,10 @@
 #/bin/sh
+rm -f /tmp/jenkins-1.0.21.zip
 if [ ! -f /var/lib/jenkins/init.txt ]; then
     echo "First time setup of Jenkins container.... this may take a few minutes"
     cd /tmp
     wget https://dl.bintray.com/jfrog/enterprise-trial-installer/jfrog/jenkins/jenkins-1.0.21.zip
     unzip jenkins-1.0.21.zip
-    mv /tmp/jfrog/data/jenkins/ /var/lib/jenkins/
+    mv -n /tmp/jfrog/data/jenkins/* /var/lib/jenkins/
     touch /var/lib/jenkins/init.txt
 fi
